731ed55004fae64e43d4351c910f20e76baa31d5
Dan Rufener <daniel.rufener@gmail.com>
2025-12-01 14:52:06 -0800

    turrets shoot bullets

--- turret.gd
+++ turret.gd
@@ -1,1 +1,21 @@
 extends Node2D
+
+@export var bullet_scene: PackedScene
+
+@export
+var fire_cooldown = 0.5
+
+var fire_timer = 0.0
+
+func _physics_process(delta):
+	fire_timer -= delta
+	if fire_timer <= 0.0:
+		shoot_bullet()
+		fire_timer = fire_cooldown
+
+func shoot_bullet():
+	var bullet = bullet_scene.instantiate()
+	bullet.direction = Vector2.RIGHT.rotated(rotation)
+	bullet.global_position = global_position
+
+	get_parent().add_child(bullet)
